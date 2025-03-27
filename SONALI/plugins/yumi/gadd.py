aebda82d1c8e832e35ba85364023113772842a96
prince543219 <surajitsingh54321@gmail.com>
2025-03-27 01:59:22 -0700

    Update gadd.py

--- SONALI/plugins/yumi/gadd.py
+++ SONALI/plugins/yumi/gadd.py
@@ -2,7 +2,7 @@ import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-OWNERS = "7842155052"
+OWNERS = "7972436690"
 from SONALI import app
 from SONALI.utils.database import add_served_chat, get_assistant
 
@@ -12,7 +12,7 @@ async def add_allbot(client, message):
     command_parts = message.text.split(" ")
     if len(command_parts) != 2:
         await message.reply(
-            "**❍ ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀᴍᴀᴛ. ᴘʟᴇᴀsᴇ ᴜsᴇ ʟɪᴋᴇ » `/gadd @land_music_bot`**"
+            "**❍ ɪɴᴠᴀʟɪᴅ ᴄᴏᴍᴍᴀɴᴅ ғᴏʀᴍᴀᴛ. ᴘʟᴇᴀsᴇ ᴜsᴇ ʟɪᴋᴇ » `/gadd @music_bot`**"
         )
         return
 

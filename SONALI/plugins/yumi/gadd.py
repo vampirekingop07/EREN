3f24e91e50ec18145a2a45720fdd9e75810cddc8
prince543219 <surajitsingh54321@gmail.com>
2025-03-27 01:57:29 -0700

    Update gadd.py

--- SONALI/plugins/yumi/gadd.py
+++ SONALI/plugins/yumi/gadd.py
@@ -3,8 +3,8 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 OWNERS = "7842155052"
-from ISTKHAR_MUSIC import app
-from ISTKHAR_MUSIC.utils.database import add_served_chat, get_assistant
+from SONALI import app
+from SONALI.utils.database import add_served_chat, get_assistant
 
 
 @app.on_message(filters.command("gadd") & filters.user(int(OWNERS)))

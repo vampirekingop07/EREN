e4eaea4d4e671ecb4da3b3deca1ce93106066ef0
Md hakeeb <mdhakeeb048@gmail.com>
2025-04-11 18:03:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/vampire-king-786/KAMINA-SONALI-REPO",
+    "https://github.com/vampire-king-786/BABYMUSICVAMPIRE",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(

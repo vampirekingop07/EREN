3435daa2d8f5a4d88ef5f6866d59adccfd91d217
vampirekingop07 <naaztravels62@gmail.com>
2025-09-30 14:43:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/va7ce/EREN",
+    "https://github.com/vampirekingop07/EREN",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
